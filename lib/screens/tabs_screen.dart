07150763699dc89c96f2a029bcb339481d5234fb
Gian Pereira <gian.pereira@magazineluiza.com.br>
2024-01-04 18:09:44 -0300

    Adicionando TabBar na AppBar

---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'categories_screen.dart';
+import 'favorite_screen.dart';
+
+class TabsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      initialIndex: 1,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Vamos Cozinhar'),
+          bottom: const TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(Icons.category),
+                text: 'Categorias',
+              ),
+              Tab(
+                icon: Icon(Icons.star),
+                text: 'Favoritas',
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            CategoriesScreen(),
+            FavoriteScreen(),
+          ],
+        ),
+      ),
+    );
+  }
+}
